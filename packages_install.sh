8ad8b8fc16edf030c9cc54117678202c22e5e17e
dhl301105 <dhl301105@gmail.com>
2025-02-03 09:25:58 +0000

    first commit

---
+++ packages_install.sh
@@ -0,0 +1,12 @@
+
+sudo pacman -Syyu
+
+sudo pacman -S fish git hyprland  wl-clipboard python3 npm btop foot  fisher  waybar hyprpaper fuzzel unzip base-devel 
+
+# sudo pacman -S dust duf dunst libnotify thermald
+
+sudo pacman -S adobe-source-han-sans-cn-fonts adobe-source-han-sans-tw-fonts adobe-source-han-sans-kr-fonts noto-fonts-emoji ttf-font-awesome
+
+chsh -s /usr/bin/fish
+
+# fisher install IlanCosman/tide@v6
