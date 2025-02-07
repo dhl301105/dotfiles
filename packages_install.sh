bdfe740cbe10dff7fc2d29b82f42a3803086e8ef
dhl301105 <dhl301105@gmail.com>
2025-02-07 09:42:40 +0700

    first commit

--- packages_install.sh
+++ packages_install.sh
@@ -1,10 +1,20 @@
 sudo pacman -Syyu
 
-sudo pacman -S fish git hyprland wl-clipboard python3 npm btop foot fisher waybar hyprpaper fuzzel unzip base-devel fd
+sudo pacman -S fish git hyprland wl-clipboard python3 npm btop foot fisher waybar hyprpaper fuzzel unzip base-devel fd rofi-wayland thermald dust duf
 
-# sudo pacman -S dust duf dunst libnotify thermald
+# sudo pacman -S dunst libnotify
 
 sudo pacman -S adobe-source-han-sans-cn-fonts adobe-source-han-sans-tw-fonts adobe-source-han-sans-kr-fonts noto-fonts-emoji ttf-font-awesome ttf-joypixels gnu-free-fonts
+
+sudo pacman -S pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber sof-firmware
+
+systemctl --user enable --now pipewire.socket
+
+systemctl --user enable --now pipewire-pulse.socket
+
+systemctl --user enable --now wireplumber.service
+
+systemctl enable --now thermald
 
 chsh -s /usr/bin/fish
 
