2d33060fd2a7a99f6eef7a2e6dd618ab5850f217
dhl301105 <dhl301105@gmail.com>
2025-02-07 07:55:24 +0700

    first commit

--- .config/nvim/lua/plugins/fzf.lua
+++ .config/nvim/lua/plugins/fzf.lua
@@ -12,7 +12,11 @@ return {
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		config = function()
 			-- calling `setup` is optional for customization
-			require("fzf-lua").setup({})
+			require("fzf-lua").setup({
+				files = {
+					fd_opts = [[--type f --hidden --exclude .git --exclude node_modules --exclude build --exclude .cache]],
+				},
+			})
 		end,
 	},
 	{ "junegunn/fzf", build = "./install --bin" },
