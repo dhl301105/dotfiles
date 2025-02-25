ed17ce7419f305025dc8945c5acb4caf6f6f118e
dhl301105 <dhl301105@gmail.com>
2025-02-25 14:36:32 +0000

    first commit

--- packages_install.sh
+++ packages_install.sh
@@ -1,6 +1,6 @@
-sudo pacman -Syyu
+sudo pacman -Syyyuu
 
-sudo pacman -S fish git hyprland wl-clipboard python3 npm btop foot waybar hyprpaper fuzzel unzip base-devel fd rofi-wayland dust duf libva-nvidia-driver
+sudo pacman -S fish git hyprland wl-clipboard python3 npm btop foot waybar hyprpaper fuzzel unzip base-devel fd rofi-wayland dust duf libva-nvidia-driver inotify-tools dpkg
 
 # sudo pacman -S dunst libnotify thermald fisher
 
