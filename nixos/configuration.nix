391650f655e106c0e905c6d201dcfec6d8372b21
dhl301105 <dhl301105@gmail.com>
2025-03-06 18:09:57 +0700

    first commit

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,27 +1,96 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
 { config, pkgs, ... }:
 
-{ 
-  programs.git = {
-    enable = true;
-    userName  = "dhl301105";
-    userEmail = "dhl301105@gmail.com";
-    extraConfig = {
-      credential.helper = "${
-          pkgs.git.override { withLibsecret = true; }
-        }/bin/git-credential-libsecret";
-    };
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.networkmanager.enable = true;
+  networking.nameservers = [ "8.8.8.8" ];
+  networking.hostName = "nixos";
+  networking.firewall.enable = true;
+	# networking.wireless.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Asia/Ho_Chi_Minh";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "vi_VN";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "vi_VN";
+    LC_IDENTIFICATION = "vi_VN";
+    LC_MEASUREMENT = "vi_VN";
+    LC_MONETARY = "vi_VN";
+    LC_NAME = "vi_VN";
+    LC_NUMERIC = "vi_VN";
+    LC_PAPER = "vi_VN";
+    LC_TELEPHONE = "vi_VN";
+    LC_TIME = "vi_VN";
   };
 
-  i18n.inputMethod = {
-    enable = true;
-    type = "ibus";
-    ibus.engines =with pkgs.ibus-engines; [ Bamboo ];
- };
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
 
-  nixpkgs.config = {
-    allowUnfree = true;
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
   };
-  
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.dhl301105 = {
+    isNormalUser = true;
+    description = "dhl301105";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+    #  thunderbird
+    ];
+  };
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
   environment.systemPackages = with pkgs; [
     neovim
     jetbrains.idea-ultimate
@@ -38,76 +107,71 @@
 		google-chrome
 		rofi-wayland
 		gimp
-		fd
-    jq
-    fzf
-    ripgrep
-    curl
+		dpkg
     wget
-    exa 
-    neofetch
-    zoxide
-    unzip
-    zip
-    gcc
-    cliphist
-    nautilus
+    curl
   ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
+
+  # programs.git = {
+  #   enable = true;
+  #   userName  = "John Doe";
+  #   userEmail = "johndoe@example.com";
+    # extraConfig = {
+    #   credential.helper = "${
+    #       pkgs.git.override { withLibsecret = true; }
+    #     }/bin/git-credential-libsecret";
+    # };
+  # };
+
+  i18n.inputMethod = {
+    enable = true;
+    type = "ibus";
+    # ibus.engines =with pkgs.ibus-engines; [ Unikey ];
+  };
 
   programs.appimage.enable = true;
   programs.appimage.binfmt = true;
 
-  services.libinput.enable = true;
-
   qt.enable = true;
-  qt.platformTheme = "gtk4";
-  qt.style = "gtk4";
-  
+  qt.platformTheme = "gnome";
+  qt.style = "adwaita-dark";
+
 	programs.hyprland.enable = true;
 
-  hardware.graphics.extraPackages = [
-    libva-utils
-  ];
-
-  networking.networkmanager.enable = true;
-  networking.nameservers = [ "8.8.8.8" ];
-  networking.hostName = "nixos";
-  networking.firewall.enable = false;
-	networking.wireless.enable = true;
-
-  services.openssh.enable = false; 
-  services.openssh.permitRootLogin = "no";
-  services.openssh.passwordAuthentication = true;
-  AllowUsers = null; # Allows all users by default. Can be [ "user1" "user2" ]
-  UseDns = true; 
-  users.users.alice.openssh.authorizedKeys.keys =
-    [ "ssh-ed25519 AAAAB3NzaC1kc3MAAACBAPIkGWVEt4..." ];
-
-  boot.kernelPatches = [
-    {
-      name = "Rust Support";
-      patch = null;
-      features = {
-        rust = true;
-      };
-    }
-  ];
-
-	time.timeZone = "Asia/Ho_Chi_Minh";
-
-  # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
+  # hardware.graphics.extraPackages = [
+  #   libva-utils
+  # ];
 
   programs.fish.enable = true;
 
-  programs.bash = { 
+  programs.bash = {
     interactiveShellInit = ''
       if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
       then
@@ -116,10 +180,5 @@
       fi
     '';
   };
-
-  hardware.bluetooth.enable = true; # enables support for Bluetooth
-  hardware.bluetooth.powerOnBoot = true; 
-  services.blueman.enable = true;
-
 
 }
