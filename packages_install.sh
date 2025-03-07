6c00f5b4659b7e929814f5956d808bfe1b429a9d
dhl301105 <dhl301105@gmail.com>
2025-03-07 14:21:28 +0700

    first commit

--- packages_install.sh
+++ packages_install.sh
@@ -1,21 +1,41 @@
-sudo pacman -Syyyuu
+if [ -f /etc/arch-release ]; then
+    OS="arch"
 
-sudo pacman -S fish git hyprland wl-clipboard python3 npm btop foot waybar hyprpaper fuzzel unzip base-devel fd rofi-wayland dust duf jq libva-nvidia-driver inotify-tools dpkg
+    echo "Detected OS: $OS"
 
-# sudo pacman -S dunst libnotify thermald fisher
+    sudo pacman -Syyyuu
 
-sudo pacman -S adobe-source-han-sans-cn-fonts adobe-source-han-sans-tw-fonts adobe-source-han-sans-kr-fonts noto-fonts-emoji ttf-font-awesome ttf-joypixels gnu-free-fonts
+    sudo pacman -S fish git hyprland wl-clipboard python3 npm btop foot waybar hyprpaper fuzzel unzip base-devel fd rofi-wayland dust duf jq libva-nvidia-driver inotify-tools dpkg
 
-sudo pacman -S pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber sof-firmware
+    # sudo pacman -S dunst libnotify thermald fisher
 
-systemctl --user enable --now pipewire.socket
+    sudo pacman -S adobe-source-han-sans-cn-fonts adobe-source-han-sans-tw-fonts adobe-source-han-sans-kr-fonts noto-fonts-emoji ttf-font-awesome ttf-joypixels gnu-free-fonts
 
-systemctl --user enable --now pipewire-pulse.socket
+    sudo pacman -S pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber sof-firmware
 
-systemctl --user enable --now wireplumber.service
+    systemctl --user enable --now pipewire.socket
 
-# systemctl enable --now thermald
+    systemctl --user enable --now pipewire-pulse.socket
 
-chsh -s /usr/bin/fish
+    systemctl --user enable --now wireplumber.service
+
+    # systemctl enable --now thermald
+
+    chsh -s /usr/bin/fish
 
 # fisher install IlanCosman/tide@v6
+elif [ -f /etc/debian_version ]; then
+    OS="debian"
+    echo "Detected OS: $OS"
+
+elif [ -f /etc/fedora-release ]; then
+    OS="fedora"
+    echo "Detected OS: $OS"
+
+    sudo dnf install --skip-unavailable fish git hyprland wl-clipboard python3 npm nodejs foot waybar hyprpaper unzip fd rofi-wayland dust duf jq
+
+    chsh -s /usr/bin/fish
+else
+    echo "Unsupported OS"
+    exit 1
+fi
