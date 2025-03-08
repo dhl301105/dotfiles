932ca4b5a30567a18eb5f581a7af66c6f18ab811
dhl301105 <dhl301105@gmail.com>
2025-03-08 20:49:08 +0700

    first commit

--- dotfiles_manager.sh
+++ dotfiles_manager.sh
@@ -29,7 +29,7 @@ if [[ ! -d "$dotfiles/.config" ]]; then
   mkdir -p "$dotfiles/.config"
 fi
 
-folders+="$@"
+folders+=$@
 
 for folder in $folders; do
   if [ "$flag_r" == "true" ] && [ -d "$dotfiles/.config/$folder" ]; then
